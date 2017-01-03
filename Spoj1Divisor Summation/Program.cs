f4f94f79050527f78d76758e40b232d06b85894c
Patryk Jargieło <jargielo.patryk@gmail.com>
2017-01-03 15:47:56 +0100

    ========== Run test finished: 1 run (0:00:00,4533081) ==========

--- Spoj1Divisor Summation/Program.cs
+++ Spoj1Divisor Summation/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,15 +16,44 @@ namespace Spoj1Divisor_Summation
             var writer = new StreamWriter(Console.OpenStandardOutput());
             writer.AutoFlush = false;
 
+
             DivisorSumation(reader, writer);
         }
 
         public static void DivisorSumation(TextReader reader, TextWriter writer)
         {
-           //*******************************
-           //* TODO: Implement method body *
-           //*******************************
+            //*******************************
+            //* TODO: Implement method body *
+            //*******************************
 
+            List<int> Inputy = new List<int>();
+            int ileRekordów = int.Parse(reader.ReadLine());
+
+            for (int i = 0; i < ileRekordów; i++)
+            {
+                int liczba = int.Parse(reader.ReadLine());
+                Inputy.Add(liczba);
+            }
+            foreach (var k in Inputy)
+
+            {
+                int suma = 0;
+                if (k >= 1 && k <= 500000)
+                {
+                    for (int i = 1; i < k; i++)
+                    {
+                        if (k != i)
+                        {
+                            int czyPodzielna = k % i;
+                            if (czyPodzielna == 0)
+                            {
+                                suma += i;
+                            }
+                        }
+                    }
+                    writer.WriteLine(suma);
+                }
+            }
             reader.Close();
             writer.Flush();
             writer.Close();
