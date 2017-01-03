1bc91a0ca72c3f9fa8d7838e5d5de8514f42d7ae
FraczkowskiC <cezaryfraczkowski@gmail.com>
2017-01-03 15:51:02 +0100

    Cezary Fraczkowski

--- Spoj1Divisor Summation/Program.cs
+++ Spoj1Divisor Summation/Program.cs
@@ -15,18 +15,49 @@ namespace Spoj1Divisor_Summation
             var writer = new StreamWriter(Console.OpenStandardOutput());
             writer.AutoFlush = false;
 
+
             DivisorSumation(reader, writer);
+            Console.ReadKey();
         }
 
         public static void DivisorSumation(TextReader reader, TextWriter writer)
         {
-           //*******************************
-           //* TODO: Implement method body *
-           //*******************************
+            //*******************************
+            //* TODO: Implement method body *
+            //*******************************
+            string inputNum;
+            string inputSize;
+            var total = 0;
+            List<int> list = new List<int>();
+            inputSize = reader.ReadLine();
+            var z = Convert.ToInt32(inputSize);
+            for (int k = 0; k < z; k++)
+            {
+                inputNum = reader.ReadLine();
+                var number = Convert.ToInt32(inputNum);
+                for (var i = 1; i <= number; i++)
+                {
+                    if (number % i == 0 && i != number)
+                    {
+                        list.Add(i);
 
+                    }
+                }
+                foreach (var item in list)
+                {
+
+                    total = 0;
+                    total = list.Sum();
+
+                }
+                list.Clear();
+                writer.WriteLine(total);
+            }
             reader.Close();
             writer.Flush();
             writer.Close();
+
         }
     }
 }
+
