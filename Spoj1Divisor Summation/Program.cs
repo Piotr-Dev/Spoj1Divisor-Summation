0a2c1821d6f2f93894d32350ae6ddf1724d27113
MajewskiP <Pawel@DESKTOP-L6N976T>
2017-01-04 14:49:08 +0100

    --refactoring

--- Spoj1Divisor Summation/Program.cs
+++ Spoj1Divisor Summation/Program.cs
@@ -13,44 +13,30 @@ namespace Spoj1Divisor_Summation
         {
             try
             {
-                //var reader = new ReaderReader(Console.OpenStandardInput()); //new StreamReader(Console.OpenStandardInput());
+                Console.WriteLine("Input:");
                 var reader = new StringReader(NumberInput());
+
+                Console.WriteLine(" \nOutput:");
                 var writer = new StreamWriter(Console.OpenStandardOutput());
                 writer.AutoFlush = false;
 
-                Console.WriteLine(" \nOutput:");
                 DivisorSumation(reader, writer);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-            }
-
+            }        
             Console.ReadKey();
         }
 
         public static void DivisorSumation(TextReader reader, TextWriter writer)
-        {
+        {           
             int numberCount = Convert.ToInt32(reader.ReadLine());
-            while (true) //do for every line of stream
-            {
-                List<int> divs = new List<int>();
+            for (int i = 0; i < numberCount; i++)
+            {            
                 int number = Convert.ToInt32(reader.ReadLine());
-
-                for (int i = 1; i < number; i++)
-                {
-                    if ((number % i) == 0)
-                    {
-                        divs.Add(i);
-                    }
-
-                }
-
-                writer.WriteLine(divs.Sum());
-                numberCount--;
-                if (numberCount == 0)
-                    break;
-
+                List<int> divisors = CalculateDivisors(number);         
+                writer.WriteLine(divisors.Sum());
             }
 
             reader.Close();
@@ -58,27 +44,39 @@ namespace Spoj1Divisor_Summation
             writer.Close();
         }
 
-        public static string NumberInput()
+        private static List<int>CalculateDivisors(int number)
         {
-            int inputNumber;
-            var input = new StringBuilder();
+            List<int> divisors = new List<int>();
+            for (int i = 1; i < number; i++)
+                if ((number % i) == 0) divisors.Add(i);
 
-            Console.WriteLine("Input:");
-            inputNumber = Convert.ToInt32(Console.ReadLine()); //input number of test cases
-            if (inputNumber < 0)
-                throw new Exception("Incorrect input");
+            return divisors;
+        }
 
-            input.AppendLine(inputNumber.ToString());
-            int counter = inputNumber;
+        public static string NumberInput()
+        {         
+            int inputCount = Convert.ToInt32(Console.ReadLine());
+            if (inputCount < 0) throw new Exception("Incorrect input");
 
-            for (int i = 0; i < counter; i++)
+            var inputNumbers = new StringBuilder();
+            inputNumbers.AppendLine(inputCount.ToString());
+
+            for (int i = 0; i < inputCount; i++)
             {
-                inputNumber = Convert.ToInt32(Console.ReadLine());
-                if (inputNumber < 1 || inputNumber > 500000)
-                    throw new Exception("Incorrect input (must be (1 <= n <= 500000))");
-                input.AppendLine(inputNumber.ToString());
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (IsValidInputNumber(number)) throw new Exception("Incorrect input (must be (1 <= n <= 500000))");
+                inputNumbers.AppendLine(number.ToString());
             }
-            return input.ToString();
+            return inputNumbers.ToString();
+        }
+
+        private static bool IsValidInputNumber(int inputNumber)
+        {
+            const int lowerLimit = 1;
+            const int upperLimit = 500000;
+            if (inputNumber < lowerLimit) return false;
+            if (inputNumber > upperLimit) return false;
+            return true;
         }
 
     }
