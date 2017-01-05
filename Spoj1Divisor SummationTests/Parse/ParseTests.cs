045bb32c013dc4491beec41e7d0706441127b355
Ziumper <tomasz.komoszeski@gmail.com>
2017-01-05 14:23:50 +0100

    Changing the folder

---
+++ Spoj1Divisor SummationTests/Parse/ParseTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Spoj1Divisor_SummationTests
+{
+    [TestClass()]
+    public class ParseTests
+    {
+        [TestMethod()]
+        public void TryParseIntCorrectnessTest()
+        {
+            var numberInString = "33333333333333333333333333333";
+            int number;
+            var isNumber = int.TryParse(numberInString,out number);
+
+            Assert.AreNotSame(isNumber,true);
+            Assert.AreEqual(number,0);
+        }
+    }
+}
