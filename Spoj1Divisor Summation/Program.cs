44e0042eb3e3515ec3903763f6910de78a30cacf
Piotr M <piotr.mielka@billennium.pl>
2017-01-09 00:43:33 +0100

    Example solution + test update

--- Spoj1Divisor Summation/Program.cs
+++ Spoj1Divisor Summation/Program.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Spoj1Divisor_Summation
 {
@@ -20,13 +18,61 @@ namespace Spoj1Divisor_Summation
 
         public static void DivisorSumation(TextReader reader, TextWriter writer)
         {
-           //*******************************
-           //* TODO: Implement method body *
-           //*******************************
+            GetDividerSums(reader)
+                .ToList()
+                .ForEach(sum => writer.WriteLine(sum));
 
             reader.Close();
             writer.Flush();
             writer.Close();
+        }
+
+        private static IEnumerable<int> GetDividerSums(TextReader reader)
+        {
+            var inputCount = GetInputedNumber(reader.ReadLine());
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                var inputNumber = GetInputedNumber(reader.ReadLine());
+
+                var dividersSum = GetDividers(inputNumber).Sum();
+
+                yield return dividersSum;
+            }
+        }
+
+        private static IEnumerable<int> GetDividers(int inputNumber)
+        {
+            for (int divider = 1; divider < inputNumber; divider++)
+            {
+                var isTrueDivider = inputNumber % divider == 0;
+
+                if (isTrueDivider)
+                {
+                    yield return divider;
+                }
+            }
+        }
+
+        private static int GetInputedNumber(string inputString)
+        {
+            var inputedNumber = int.Parse(inputString);
+
+            ValidateNumericInput(inputedNumber);
+
+            return inputedNumber;
+        }
+
+        private static void ValidateNumericInput(int input)
+        {
+            const int numericInputLowerRange = 0;
+            const int numericInputUpperRange = 50000;
+
+            var isOutOfRange = input < numericInputLowerRange ||
+                               input > numericInputUpperRange;
+
+            if (isOutOfRange)
+                throw new ArgumentOutOfRangeException($"Expected range is ({numericInputLowerRange} , {numericInputUpperRange}) ");
         }
     }
 }
